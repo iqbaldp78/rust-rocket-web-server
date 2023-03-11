c857df0cc50648f5ff633dd8595e05a580116df3
iqbal d prawira <iqbaldp78@gmail.com>
2023-03-11 20:20:12 +0700

    init project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, worlds!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index,world]).launch();
+}
