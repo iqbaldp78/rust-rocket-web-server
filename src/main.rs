f2577a306b4e4cde0d4f2d1c636ff70a0e96347b
iqbal.prawira <iqbaldp78@gmail.com>
2023-03-12 09:18:08 +0700

    add blocking task

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 
 #[macro_use] extern crate rocket;
 
+use std::io;
+
+use rocket::tokio::task::spawn_blocking;
+use rocket::tokio::time::{sleep, Duration};
+
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -12,6 +18,32 @@ fn world() -> &'static str {
     "Hello, worlds!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index,world]).launch();
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build()
+        .mount("/", routes![index])
+        .mount("/hello", routes![world,delay,blocking_task])
+        .launch()
+        .await?;
+
+    Ok(())
+}
+
+
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    println!("hert");
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[get("/blocking_task")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    // In a real app, use rocket::fs::NamedFile or tokio::fs::File.
+    let vec = spawn_blocking(|| std::fs::read("data.txt")).await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+    Ok(vec)
 }
